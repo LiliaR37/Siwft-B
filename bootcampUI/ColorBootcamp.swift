53bb32dc594d156048fe81a778bbe08e1a1c18b0
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-16 15:00:46 -0300

    Color

--- bootcampUI/ColorBootcamp.swift
+++ bootcampUI/ColorBootcamp.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct ColorBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25.0)
+            .fill(
+                //Color.blue
+                //Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1))
+                //Color(UIColor.secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+            //.shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, x: -20, y: -20)
     }
 }
 
 struct ColorBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         ColorBootcamp()
+            
+            
     }
 }
